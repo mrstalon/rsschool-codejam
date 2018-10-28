5544b3a901aeb738f569533a7dd6fb8c377a8c46
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2018-10-28 14:26:27 +0300

    feat: add tests for make function

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const sumOfOther = require('../solution/sumOfOther.js');
+const make = require('../solution/make.js');
 
 
 describe('sumOfOther', () => {
@@ -11,5 +12,22 @@ describe('sumOfOther', () => {
   });
   it('should return converted array correctly 3', () => {
     assert.deepEqual(sumOfOther([1, 5, 10, 3, 7]), [25, 21, 16, 23, 19]);
+  });
+});
+
+describe('make', () => {
+  const sum = (a, b) => a + b;
+  const dif = (a, b) => a - b;
+  it('should return sum of all arguments correctly 1', () => {
+    assert.equal(make(1, 3)(6, 5, 7)(4)(sum), 26);
+  });
+  it('should return sum of all arguments correctly 2', () => {
+    assert.equal(make(10)(25, 33)(1)(5)(101)(sum), 175);
+  });
+  it('should return difference between all agruments in correct order(left -> right) 1', () => {
+    assert.equal(make(30, 5)(15)(4, 4, 2)(dif), 0);
+  });
+  it('should return difference between all agruments in correct order(left -> right) 1', () => {
+    assert.equal(make(666)(111)(222, 333)(111)(dif), -111);
   });
 });
