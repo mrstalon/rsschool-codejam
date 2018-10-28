a94b0ccda872125a8d9e06047a600d5d4e7cfd32
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2018-10-28 15:01:20 +0300

    feat: implement recursion function and tests for it

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const sumOfOther = require('../solution/sumOfOther.js');
 const make = require('../solution/make.js');
+const recursion = require('../solution/recursion.js');
 
 
 describe('sumOfOther', () => {
@@ -29,5 +30,98 @@ describe('make', () => {
   });
   it('should return difference between all agruments in correct order(left -> right) 1', () => {
     assert.equal(make(666)(111)(222, 333)(111)(dif), -111);
+  });
+});
+
+describe('recursion', () => {
+  it('should return correct array containing tree values 1', () => {
+    const tree = {
+      value: 10,
+      left: {
+        value: 5,
+      },
+      right: {
+        value: 15,
+      },
+    };
+    assert.deepEqual(recursion(tree), [[10], [5, 15]]);
+  });
+  it('should return correct array containing tree values 2', () => {
+    const tree = {
+      value: 10,
+      left: {
+        value: 5,
+        left: {
+          value: 3,
+        },
+        right: {
+          value: 6,
+        },
+      },
+      right: {
+        value: 15,
+        left: {
+          value: 13,
+        },
+        right: {
+          value: 18,
+        },
+      },
+    };
+    assert.deepEqual(recursion(tree), [[10], [5, 15], [3, 6, 13, 18]]);
+  });
+  it('should return correct array containing tree values 3', () => {
+    const tree = {
+      value: 10,
+      left: {
+        value: 5,
+        left: {
+          value: 3,
+          left: {
+            value: 1,
+          },
+        },
+        right: {
+          value: 6,
+        },
+      },
+      right: {
+        value: 15,
+        left: {
+          value: 13,
+        },
+        right: {
+          value: 18,
+          right: {
+            value: 20,
+          },
+        },
+      },
+    };
+    assert.deepEqual(recursion(tree), [[10], [5, 15], [3, 6, 13, 18], [1, 20]]);
+  });
+  it('should return correct array containing tree values 4', () => {
+    const tree = {
+      value: 100,
+      left: {
+        value: 90,
+        left: {
+          value: 70,
+        },
+        right: {
+          value: 99,
+        },
+      },
+      right: {
+        value: 120,
+        left: {
+          value: 110,
+        },
+        right: {
+          value: 130,
+        },
+      },
+    };
+    assert.deepEqual(recursion(tree), [[100], [90, 120], [70, 99, 110, 130]]);
   });
 });
